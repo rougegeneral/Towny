be53a2ac1bfe9a7eef191fc2dee665deff13eb4e
Suneet Tipirneni <suneettipirneni@icloud.com>
2020-01-16 00:03:43 -0500

    Made the CreateResult enum have a new description field.

--- src/com/palmergames/bukkit/towny/database/results/CreateResult.java
+++ src/com/palmergames/bukkit/towny/database/results/CreateResult.java
@@ -5,8 +5,19 @@ package com.palmergames.bukkit.towny.database.results;
  * @author Suneet Tipirneni (Siris)
  */
 public enum CreateResult {
-	UNEXPECTED,
-	ALREADY_EXISTS,
-	SUCCESSFUL,
-	INVALID_NAME,
+	UNEXPECTED("Unexpected Error Occurred, while creating."),
+	ALREADY_EXISTS("The specified data already exists."),
+	SUCCESSFUL("The operation completed successfully."),
+	INVALID_NAME("The name given for the data is not valid.");
+	
+	// A description for logging purposes mainly.
+	String description;
+	
+	CreateResult(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
 }
